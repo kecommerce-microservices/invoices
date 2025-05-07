bb02bf7e8f366f12572f185ed03621663ccebc1b
Kauã Pereira <zkauacodes@hotmail.com>
2025-05-06 22:14:27 -0300

    Initial commit

--- infrastructure/src/main/java/com.devkit.invoices/infrastructure/rest/controllers/Ping.java
+++ infrastructure/src/main/java/com.devkit.invoices/infrastructure/rest/controllers/Ping.java
@@ -1,6 +1,8 @@
 package com.devkit.invoices.infrastructure.rest.controllers;
 
 import com.devkit.invoices.domain.utils.InstantUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +15,11 @@ import java.util.Map;
 @RequestMapping("/ping")
 public class Ping {
 
+    private static final Logger log = LoggerFactory.getLogger(Ping.class);
+
     @GetMapping
     public ResponseEntity<?> pong() {
+        log.info("pong");
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(Map.of("Pong", InstantUtils.now().toString()));
