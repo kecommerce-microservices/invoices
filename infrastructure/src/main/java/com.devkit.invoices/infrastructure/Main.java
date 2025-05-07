955705bd58d507ba00bd47fcbaec1cac9a119ef5
Kauã Pereira <zkauacodes@hotmail.com>
2025-05-06 23:09:13 -0300

    Initial commit

--- infrastructure/src/main/java/com.devkit.invoices/infrastructure/Main.java
+++ infrastructure/src/main/java/com.devkit.invoices/infrastructure/Main.java
@@ -1,5 +1,7 @@
 package com.devkit.invoices.infrastructure;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.AbstractEnvironment;
@@ -7,8 +9,11 @@ import org.springframework.core.env.AbstractEnvironment;
 @SpringBootApplication
 public class Main {
 
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(Main.class, args);
+        log.info("STARTED");
     }
 }
